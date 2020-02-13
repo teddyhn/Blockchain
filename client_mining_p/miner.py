f6ad672fbeb7f535158a58c0153a2408fe9b33f1
teddyhn <tngo97@gmail.com>
2020-02-13 11:35:51 -0800

    lecture changes

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -36,7 +36,7 @@ def valid_proof(block_string, proof):
     guess = f'{block_string}{proof}'.encode()
     guess_hash = hashlib.sha256(guess).hexdigest()
 
-    return guess_hash[:6] == "000000"
+    return guess_hash[:3] == "000"
 
 
 if __name__ == '__main__':
